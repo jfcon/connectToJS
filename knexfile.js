70024c1969c3699edd0a9fdda34bd51b5e207821
John <connollyjohnf@gmail.com>
2019-02-27 00:20:09 +0000

    Migration of milestones table in test_db

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,54 @@
 // Update with your config settings.
 
-module.exports = {
+const settings = require("./settings"); // settings.json
 
+module.exports = {
   development: {
-    client: 'sqlite3',
+    client: "pg",
     connection: {
-      filename: './dev.sqlite3'
+      host: settings.hostname,
+      user: settings.user,
+      password: settings.password,
+      database: settings.database
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
     }
   },
 
   staging: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: "my_db",
+      user: "username",
+      password: "password"
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: "knex_migrations"
     }
   },
 
   production: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: "my_db",
+      user: "username",
+      password: "password"
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: "knex_migrations"
     }
   }
-
 };
